d4f09777c091a91e642d8b8965a7a06dbafda803
RBLG <37543249+RBLG@users.noreply.github.com>
2024-10-23 14:54:26 +0200

    block update done

--- src/main/java/teluri/mods/jlrays/ConeTracedLightEngine.java
+++ src/main/java/teluri/mods/jlrays/ConeTracedLightEngine.java
@@ -12,7 +12,6 @@ import net.minecraft.world.level.chunk.LightChunk;
 import net.minecraft.world.level.chunk.LightChunkGetter;
 import net.minecraft.world.level.lighting.LayerLightEventListener;
 import teluri.mods.jlrays.ConeTracer26Nbs.ILightConsumer;
-import teluri.mods.jlrays.ConeTracer26Nbs.IOpacityGetter;
 import teluri.mods.jlrays.boilerplate.ShinyBlockPos;
 import net.minecraft.world.level.lighting.BlockLightEngine;
 
@@ -44,11 +43,11 @@ public class ConeTracedLightEngine extends BlockLightEngine implements LayerLigh
 
 	@Override
 	public boolean hasLightWork() {
-		return !this.changeQueue.isEmpty(); // TODO
+		return !this.changeQueue.isEmpty(); // TODO this.storage.hasInconsistencies() ||
 	}
 
 	@Override
-	public int runLightUpdates() { // TODO Auto-generated method stub
+	public int runLightUpdates() {
 
 		while (2 <= changeQueue.size()) {
 			long packedpos = changeQueue.dequeueLong();
@@ -63,7 +62,7 @@ public class ConeTracedLightEngine extends BlockLightEngine implements LayerLigh
 		return 0; // return value is unused anyway
 	}
 
-	protected void checkNode(long packedPos, long packedEmit) { // TODO
+	protected void checkNode(long packedPos, long packedEmit) {
 		long secpos = SectionPos.blockToSection(packedPos);
 		this.mutablePos.set(packedPos);
 
@@ -72,58 +71,55 @@ public class ConeTracedLightEngine extends BlockLightEngine implements LayerLigh
 
 		if (this.storage.storingLightForSection(secpos)) {
 			BlockState blockState = this.getState(mutablePos);
-			int newopacity = blockState.useShapeForLightOcclusion() ? 1 : 0;
+			int newopacity = getOpacity(blockState, gettermutpos) == 15 ? 0 : 1; // 1 == air
 			int oldlval = this.storage.getStoredLevel(packedPos);
 			this.storage.setStoredLevel(packedPos, newemit);
 			if (oldemit != 0 || newemit != 0) {
 				UpdateLightForSourceChanges(mutablePos, oldemit, newemit);
 			}
-			int opadiff = Integer.signum(oldlval) - newopacity;
+			int opadiff = newopacity - Integer.signum(oldlval);
 			if (opadiff != 0) {
-				UpdateLightForOpacityChange(mutablePos, opadiff);
+				UpdateLightForOpacityChange(mutablePos, Integer.signum(oldlval), newopacity);
 			}
 		}
 	}
 
 	private MutableBlockPos gettermutpos = new MutableBlockPos();// HACK this forbid paralelization of the cones
 
-	public void UpdateLightForSourceChanges(BlockPos pos, int oldemit, int newemit) {// TODO
+	public void UpdateLightForSourceChanges(BlockPos pos, int oldemit, int newemit) {
 		Vector3i source = new Vector3i(pos.getX(), pos.getY(), pos.getZ());
 		int range = 20;
 
-		IOpacityGetter opprov = (x, y, z) -> {
-			this.gettermutpos.set(x, y, z);
-			BlockState blockState = this.getState(gettermutpos);
-			blockState.canOcclude();
-			return blockState.canOcclude() ? 0 : 1;
-			// 1 - this.getOpacity(blockState, this.gettermutpos);
-		};
-
-		ILightConsumer consu = (x, y, z, visi, dist) -> {
-			long longpos = BlockPos.asLong(x, y, z);
-			if (!this.storage.storingLightForSection(SectionPos.blockToSection(longpos))) {
-				return;
-			}
-			double dval = visi * 4 / dist.length();
-			int nival = (int) Math.clamp(dval * newemit - 2, 0, 15);
-			int oival = (int) Math.clamp(dval * oldemit - 2, 0, 15);
-
-			int oldlevel = this.storage.getStoredLevel(longpos);
-
-			int newlevel = Math.clamp(oldlevel - oival + nival, 0, 15);
-			this.storage.setStoredLevel(longpos, newlevel);
-
-		};
-
-		teluri.mods.jlrays.ConeTracer26Nbs.TraceAllCones(source, range, opprov, consu);
+		ILightConsumer consu = (x, y, z, visi, dist) -> updateLight(x, y, z, visi, dist, oldemit, newemit, 0.8f);
+		ConeTracer26Nbs.TraceAllCones(source, range, this::getOpacity, consu);
 	}
 
-	public void UpdateLightForOpacityChange(BlockPos pos, int opacitydiff) {
-		// TODO
+	private MutableBlockPos gettermutpos2 = new MutableBlockPos();
+
+	public void UpdateLightForOpacityChange(BlockPos pos, int oldopa, int newopa) {
+		Vector3i origin = new Vector3i(pos.getX(), pos.getY(), pos.getZ());
+		int range = 20;
+
+		ILightConsumer consu1 = (x, y, z, visi, dist) -> {
+			this.gettermutpos2.set(x, y, z);
+			BlockState blockState = this.getState(gettermutpos2);
+			int sourceEmission = blockState.getLightEmission();
+			if (sourceEmission != 0) { // TODO add range check
+
+				ILightConsumer consu2 = (x2, y2, z2, visi2, dist2) -> {
+					this.updateLight(x2, y2, z2, visi * visi2, dist2, oldopa * sourceEmission, newopa * sourceEmission, 0.8f);
+				};
+				consu2.consumer(origin.x, origin.y, origin.z, 1, dist);
+
+				Vector3i offset = new Vector3i(origin).sub(x, y, z);
+				ConeTracer26Nbs.TraceChangeCone(origin, offset, range, this::getOpacity, consu2);
+			}
+		};
+		ConeTracer26Nbs.TraceAllCones(origin, range, this::getOpacity, consu1);
 	}
 
 	@Override
-	public void propagateLightSources(ChunkPos chunkPos) { // TODO Auto-generated method stub
+	public void propagateLightSources(ChunkPos chunkPos) {
 		this.setLightEnabled(chunkPos, true);
 		LightChunk lightChunk = this.chunkSource.getChunkForLighting(chunkPos.x, chunkPos.z);
 		if (lightChunk != null) {
@@ -132,6 +128,28 @@ public class ConeTracedLightEngine extends BlockLightEngine implements LayerLigh
 				this.UpdateLightForSourceChanges(blockPos, i, 0);
 			});
 		}
+	}
+
+	private float getOpacity(int x, int y, int z) { // this.shapeOccludes(packedPos, blockState, l, blockState2, direction)
+		this.gettermutpos.set(x, y, z);
+		BlockState blockState = this.getState(gettermutpos);
+		return getOpacity(blockState, gettermutpos) == 15 && blockState.getLightEmission() == 0 ? 0 : 1;
+	}
+
+	private void updateLight(int x, int y, int z, float visi, double dist, float oldemit, float newemit, float decayrate) {
+		long longpos = BlockPos.asLong(x, y, z);
+		if (!this.storage.storingLightForSection(SectionPos.blockToSection(longpos))) {
+			return;
+		}
+		// TODO try val/(1+val) *15 ?
+		double len = dist * decayrate;
+		int nival = (int) Math.clamp(visi * Math.max(newemit - len, 0), 0, 15);
+		int oival = (int) Math.clamp(visi * Math.max(oldemit - len, 0), 0, 15);
+
+		int oldlevel = this.storage.getStoredLevel(longpos);
+
+		int newlevel = Math.clamp(oldlevel - oival + nival, 0, 15);
+		this.storage.setStoredLevel(longpos, newlevel);
 	}
 
 	//////////////////////////////////////////////////////////////////////////
--- src/main/java/teluri/mods/jlrays/ConeTracer26Nbs.java
+++ src/main/java/teluri/mods/jlrays/ConeTracer26Nbs.java
@@ -5,16 +5,22 @@ import org.joml.Vector3i;
 public class ConeTracer26Nbs {
 
 	private static final int[] SIGNS = new int[] { 1, -1 };
+	// positive axis
 	private static final Vector3i X = new Vector3i(1, 0, 0);
 	private static final Vector3i Y = new Vector3i(0, 1, 0);
 	private static final Vector3i Z = new Vector3i(0, 0, 1);
+	// negative axis
+	private static final Vector3i NX = new Vector3i(-1, 0, 0);
+	private static final Vector3i NY = new Vector3i(0, -1, 0);
+	private static final Vector3i NZ = new Vector3i(0, 0, -1);
+	// unsigned cones
 	private static final UCone[] UCONES = new UCone[] { //
 			new UCone(X, Y, Z, 1, 0), new UCone(X, Z, Y, 0, 1), //
 			new UCone(Y, X, Z, 0, 0), new UCone(Y, Z, X, 0, 1), //
 			new UCone(Z, X, Y, 1, 1), new UCone(Z, Y, X, 1, 0),//
 	};
-
-	private static final Cone[] cones = genCones();
+	// all 48 cones
+	private static final Cone[] CONES = genCones();
 
 	private static Cone[] genCones() {
 		Cone[] ncones = new Cone[48];
@@ -36,31 +42,76 @@ public class ConeTracer26Nbs {
 	}
 	/////////////////////////////////////////////////////////////////////
 
+	private static final Offset ZERO = new Offset(0, 0, 0);
+
 	/**
-	 * trace all 48 cones
+	 * trace all 48 cones around a source
 	 * 
 	 * @see ConeTracer26Nbs.TraceCone
 	 */
-	public static void TraceAllCones(Vector3i source, int range, IOpacityGetter opmap, ILightConsumer lmap) {
-		for (Cone cone : cones) {// 48 times
-			TraceCone(source, range, cone, opmap, lmap);
+	public static void TraceAllCones(Vector3i source, int range, IAlphaProvider opmap, ILightConsumer lmap) {
+		for (Cone cone : CONES) {// 48 times
+			TraceCone(source, ZERO, range, cone, opmap, lmap);
 		}
+	}
+
+	/**
+	 * trace a 1-6 cones with an offset. this offseted cone correspond to the changes of visibility values caused by changes of a single block
+	 * 
+	 * @param origin
+	 * @param offset
+	 * @param range
+	 * @param aprov
+	 * @param lcons
+	 */
+	public static void TraceChangeCone(Vector3i origin, Vector3i offset, int range, IAlphaProvider aprov, ILightConsumer lcons) {
+		Vector3i v1 = offset.x < 0 ? NX : X;
+		Vector3i v2 = offset.y < 0 ? NY : Y;
+		Vector3i v3 = offset.z < 0 ? NZ : Z;
+		int o1 = Math.abs(offset.x);
+		int o2 = Math.abs(offset.y);
+		int o3 = Math.abs(offset.z);
+
+		// ordering high to low
+		if (o1 < o2) { // a = a ^ b ^ (b = a) == swap(a,b)
+			o1 = o1 ^ o2 ^ (o2 = o1);
+			Vector3i vtmp = v1;
+			v1 = v2;
+			v2 = vtmp;
+		}
+		if (o2 < o3) {
+			o2 = o2 ^ o3 ^ (o3 = o2);
+			Vector3i vtmp = v2;
+			v2 = v3;
+			v3 = vtmp;
+		}
+		if (o1 < o2) {
+			o1 = o1 ^ o2 ^ (o2 = o1);
+			Vector3i vtmp = v1;
+			v1 = v2;
+			v2 = vtmp;
+		}
+		Offset offset2 = new Offset(o1, o2, o3);
+		Cone cone = new Cone(v1, v2, v3, false, false, false, false); // false mean priority
+		TraceCone(origin, offset2, range, cone, aprov, lcons);
+		// TODO this should do more than one cone in case o1==o2 and/or o2==3 and/or or3==0
+		// but franckly i have no idea how to do that, especially considering that it should handle edge priority, which is done manually rn
 	}
 
 	/**
 	 * compute visibility (and light received) over one cone
 	 * 
-	 * @param source:   source of the light
+	 * @param origin:   origin of the cone
+	 * @param offset:   offset between the origin and the source
 	 * @param emit:     emition of the source
 	 * @param range:    how far should visibility by computer (to be estimated based on emit)
 	 * @param v1,v2,v3: axises vectors
 	 * @param opmap:    opacity provider
 	 * @param lmap      visibility/light value consumer
 	 */
-	public static void TraceCone(Vector3i source, int range, Cone cone, IOpacityGetter opmap, ILightConsumer lmap) {
+	public static void TraceCone(Vector3i origin, Offset offset, int range, Cone cone, IAlphaProvider opmap, ILightConsumer lmap) {
 		final Vector3i vit1 = new Vector3i();
 		final Vector3i vit2 = new Vector3i();
-		final Vector3i sdist = new Vector3i();
 		final Vector3i xyz = new Vector3i();
 
 		// store the visibility values
@@ -74,26 +125,31 @@ public class ConeTracer26Nbs {
 			for (int it2 = it1; 0 <= it2; it2--) {// start from the end to handle vbuffer values turnover easily
 				vit2.set(cone.axis2).mul(it2).add(vit1);
 				for (int it3 = it2; 0 <= it3; it3--) { // same than it2
-					sdist.set(cone.axis3).mul(it3).add(vit2); // signed distance
-					xyz.set(source).add(sdist); // world position
+					xyz.set(cone.axis3).mul(it3).add(vit2).add(origin); // signed distance
+					// xyz.set(origin).add(sdist); // world position
 
-					float opacity = opmap.getOpacity(xyz.x, xyz.y, xyz.z);
+					float opacity = opmap.get(xyz.x, xyz.y, xyz.z);
 					if (opacity == 0) {
-						vbuffer[it2][it3] = -0.0f; // below zero values mean shadow are larger around edges
+						vbuffer[it2][it3] = -0.0f; // below zero values mean shadows are larger around edges
 						continue;
 					}
-					// weights
-					int w1 = it1 - it2;
-					int w2 = it2 - it3;
-					int w3 = it3;
+					// weights adjustments to reflect the source position rather than the origin of the cone
+					int of1 = offset.o1 - offset.o2;
+					int of2 = offset.o2 - offset.o3;
+					int of3 = offset.o3;
 
-					// neigbors * their weights
-					float nb1 = w1 == 0 ? 0 : vbuffer[it2 + 0][it3 + 0] * w1;
-					float nb2 = w2 == 0 ? 0 : vbuffer[it2 - 1][it3 + 0] * w2;
-					float nb3 = w3 == 0 ? 0 : vbuffer[it2 - 1][it3 - 1] * w3;
+					// weights
+					int w1 = it1 - it2 + of1;
+					int w2 = it2 - it3 + of2;
+					int w3 = it3 + of3;
+
+					// neigbors
+					float nb1 = w1 == of1 ? 0 : vbuffer[it2 + 0][it3 + 0] * w1;
+					float nb2 = w2 == of2 ? 0 : vbuffer[it2 - 1][it3 + 0] * w2;
+					float nb3 = w3 == of3 ? 0 : vbuffer[it2 - 1][it3 - 1] * w3;
 
 					// interpolating. it1 = b1 + b2 + b3
-					float visi = /* opacity* */ (nb1 + nb2 + nb3) / it1;
+					float visi = /* opacity* */ (nb1 + nb2 + nb3) / (it1 + offset.o1);
 					visi = Math.max(visi, 0);
 					// replace the nb1 neigbor (as this pos was the last time it was needed, it can be replaced)
 					vbuffer[it2][it3] = visi;
@@ -110,7 +166,9 @@ public class ConeTracer26Nbs {
 					}
 
 					// light effects and output
-					lmap.Add(xyz.x, xyz.y, xyz.z, visi, sdist);
+					// udist.set(it1, it2, it3).add(offset.o1, offset.o2, offset.o3);
+					double dist = Vector3i.length(it1 + offset.o1, it2 + offset.o2, it3 + offset.o3);
+					lmap.consumer(xyz.x, xyz.y, xyz.z, visi, dist);
 				}
 			}
 			if (!nonzero) {
@@ -121,52 +179,28 @@ public class ConeTracer26Nbs {
 	}
 
 	@FunctionalInterface
-	public static interface IOpacityGetter {
-		float getOpacity(int x, int y, int z);
+	public static interface IAlphaProvider { // or opacity, but alpha is shorter. 0=opaque, 1=air
+		float get(int x, int y, int z);
 	}
 
 	@FunctionalInterface
 	public static interface ILightConsumer {
-		void Add(int x, int y, int z, float value, Vector3i signedDistance);
+		void consumer(int x, int y, int z, float value, double distance);
 	}
 
-	private static class Cone {
-		public final Vector3i axis1;
-		public final Vector3i axis2;
-		public final Vector3i axis3;
-		public final boolean edge1;
-		public final boolean edge2;
-		// public final boolean edge3;
-		// quadrant edge
-		public final boolean qedge2;
-		public final boolean qedge3;
+	private static record Cone(// signed cone
+			Vector3i axis1, Vector3i axis2, Vector3i axis3, //
+			boolean edge1, boolean edge2, // diagonal edges priorities
+			boolean qedge2, boolean qedge3 // quadrant edges priorities
+	) {}
 
-		public Cone(Vector3i naxis1, Vector3i naxis2, Vector3i naxis3, boolean nedge1, boolean nedge2, boolean nqedge2, boolean nqedge3) {
-			axis1 = naxis1;
-			axis2 = naxis2;
-			axis3 = naxis3;
-			edge1 = nedge1;
-			edge2 = nedge2;
-			// edge3 = nedge3;
-			qedge2 = nqedge2;
-			qedge3 = nqedge3;
-		}
-	}
-
-	private static class UCone {
-		public final Vector3i axis1;
-		public final Vector3i axis2;
-		public final Vector3i axis3;
-		public final boolean edge1;
-		public final boolean edge2;
+	private static record UCone(Vector3i axis1, Vector3i axis2, Vector3i axis3, boolean edge1, boolean edge2) { // unsigned cone
 
 		public UCone(Vector3i naxis1, Vector3i naxis2, Vector3i naxis3, int nedge1, int nedge2) {
-			axis1 = naxis1;
-			axis2 = naxis2;
-			axis3 = naxis3;
-			edge1 = nedge1 == 0;
-			edge2 = nedge2 == 0;
+			this(naxis1, naxis2, naxis3, nedge1 == 0, nedge2 == 0);
 		}
 	}
+
+	private static record Offset(int o1, int o2, int o3) {}
 
 }
