00089bf2a3e283e3ff5c159dfc6f2205cf78757c
RBLG <37543249+RBLG@users.noreply.github.com>
2025-03-11 00:33:43 +0100

    fixed incorrect direction

--- src/main/java/teluri/mods/jlrays/light/JlrBlockLightEngine.java
+++ src/main/java/teluri/mods/jlrays/light/JlrBlockLightEngine.java
@@ -332,8 +332,8 @@ public class JlrBlockLightEngine {
 			Direction d2 = 0 < quadr.axis2().y ? Direction.DOWN : Direction.UP;
 			Direction d3 = 0 < quadr.axis3().z ? Direction.NORTH : Direction.SOUTH;
 			Direction d4 = d1.getOpposite();
-			Direction d5 = d1.getOpposite();
-			Direction d6 = d1.getOpposite();
+			Direction d5 = d2.getOpposite();
+			Direction d6 = d3.getOpposite();
 
 			BlockPos curpos = new BlockPos(xyz.x, xyz.y, xyz.z);
 
