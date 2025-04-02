23dc5c4b2e28d2e5c493aa4be1eae5084c1a2bb9
RBLG <37543249+RBLG@users.noreply.github.com>
2025-04-02 17:19:49 +0200

    added functionnal interface annotation

--- src/main/java/teluri/mods/jlrays/misc/light/IBlockStateProvider.java
+++ src/main/java/teluri/mods/jlrays/misc/light/IBlockStateProvider.java
@@ -3,6 +3,7 @@ package teluri.mods.jlrays.misc.light;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
 
+@FunctionalInterface
 public interface IBlockStateProvider {
 	BlockState get(BlockPos pos);
 }
