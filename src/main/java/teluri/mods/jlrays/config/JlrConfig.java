ea199c068669c50420ddc4e398811ef3d841b719
RBLG <37543249+RBLG@users.noreply.github.com>
2025-09-12 18:28:36 +0200

    upd comments in JlrConfig

--- src/main/java/teluri/mods/jlrays/config/JlrConfig.java
+++ src/main/java/teluri/mods/jlrays/config/JlrConfig.java
@@ -17,12 +17,24 @@ public class JlrConfig {
 		return settings == null ? (settings = new JlrConfig()) : settings;
 	}
 
+	/**
+	 * memory allocated to individual light level values
+	 */
 	public IDepthHandler depthHandler = IDepthHandler.BYTE;
 
+	/**
+	 * bits before the point in the light level fixed point number. allow better quality when sources are stacking
+	 */
 	public int precision = 0;
 
+	/***
+	 * distance multiplier for light falloff
+	 */
 	public float distanceRatio = 0.1f;
 
+	/**
+	 * flat malus on light level value (pre rounding). allow values to hit 0 and so have a finite range
+	 */
 	public final float minimumValue = 0.5f;
 
 	public JlrConfig() {
