33b7b4acedf5b79d512dd926c7eaa5cc62709cd2
RBLG <37543249+RBLG@users.noreply.github.com>
2025-09-12 19:25:35 +0200

    upd comments + cleanup

--- src/main/java/teluri/mods/jlrays/config/BlockConfig.java
+++ src/main/java/teluri/mods/jlrays/config/BlockConfig.java
@@ -20,6 +20,10 @@ public class BlockConfig {
 	 */
 	private static BlockConfig settings = null;
 
+	/**
+	 * initialize config if 1st call and return it
+	 * @return
+	 */
 	public static BlockConfig LazyGet() {
 		return settings == null ? (settings = new BlockConfig()) : settings;
 	}
@@ -31,12 +35,13 @@ public class BlockConfig {
 	// will be valid once all blockstates are initialized (so on world loading should be fine)
 	public float maxEmission = 0;
 
-	public float maxRange = 15;
-
 	public BlockConfig() {
 		addDefaultPatches();
 	}
 
+	/**
+	 * modify lava properties for performance reasons
+	 */
 	private void addDefaultPatches() {
 		on("block.minecraft.lava").addLightPatch((bs, bsep) -> {
 			bsep.setLightEmit(10);
@@ -44,18 +49,31 @@ public class BlockConfig {
 		});
 	}
 
+	/**
+	 * get the highest emit to know how far to scout for sources on block updates
+	 * @return
+	 */
 	public float getMaxEmission() {
 		return maxEmission;
 	}
 
+	/**
+	 * tells the config that blockstate initialization started and further changes wont be used
+	 */
 	public void notifyInitCache() {
 		late = true;
 	}
 
+	/**
+	 * use as on(...).addPatch(...); to add a patch to one or multiple blockstates
+	 */
 	public Builder on(String... keys) {
 		return new Builder(keys);
 	}
 
+	/** 
+	 * builder for blockstate patches
+	 */
 	public class Builder {
 		final String[] keys;
 
@@ -63,6 +81,9 @@ public class BlockConfig {
 			keys = nkeys;
 		}
 
+		/**
+		 * add a barebone patch on a blockstatebase
+		 */
 		public void addPatch(Consumer<BlockStateBase> bsmod) {
 			if (late) {
 				JustLikeRays.LOGGER.warn("config modified after blockstates init started, consider using an entrypoint of type \"jlr-config\"");
@@ -71,7 +92,10 @@ public class BlockConfig {
 				blockstates.computeIfAbsent(key, (v) -> new ArrayList<>()).add(bsmod);
 			}
 		}
-
+		
+		/**
+		 * add a patch on a blockstatebase with included access to blockstate emit and opacity
+		 */
 		public void addLightPatch(LightPropertiesPatch bsmod) {
 			this.addPatch(bsmod);
 		}
