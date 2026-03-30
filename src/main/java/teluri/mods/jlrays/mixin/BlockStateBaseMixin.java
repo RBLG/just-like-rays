9a1f567b44eb44e17c373c62d014250c3dbaa243
RBLG <37543249+RBLG@users.noreply.github.com>
2026-03-30 14:28:12 +0200

    simplified dummy constructor

--- src/main/java/teluri/mods/jlrays/mixin/BlockStateBaseMixin.java
+++ src/main/java/teluri/mods/jlrays/mixin/BlockStateBaseMixin.java
@@ -9,13 +9,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.mojang.serialization.MapCodec;
-
-import it.unimi.dsi.fastutil.objects.Reference2ObjectArrayMap;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateHolder;
-import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.material.FluidState;
 import teluri.mods.jlrays.JustLikeRays;
 import teluri.mods.jlrays.config.BlockConfig;
@@ -73,8 +69,8 @@ public class BlockStateBaseMixin extends StateHolder<Block, BlockState> implemen
 	/**
 	 * fake constructor for java compiler sake
 	 */
-	protected BlockStateBaseMixin(Block owner, Reference2ObjectArrayMap<Property<?>, Comparable<?>> values, MapCodec<BlockState> propertiesCodec) {
-		super(owner, values, propertiesCodec);
+	protected BlockStateBaseMixin() {
+		super(null, null, null);
 	}
 
 }
